f42030b1a277ef84405ae87a9ff128e209835574
André Vaz <andre.vaz1411@gmail.com>
2020-12-19 16:47:46 +0000

    file namming

--- src/moduloC/ModuloC.c
+++ src/moduloC/ModuloC.c
@@ -1,5 +1,7 @@
 #include "ModuloC.h"
 
+#define MAX_FILENAME 100
+
 /**
  * @[número_de_blocos]
  * @[tamanho_bloco_1]
@@ -9,17 +11,20 @@
  * @(sequência_de_bits_resultante_da_codificação_SF_do_último_bloco)
  */
 
-void moduloC(char *filename){
+void moduloC(char *main_file){
     clock_t start_time = clock();
     
+    char shaf_file[MAX_FILENAME] = "", cod_file[MAX_FILENAME] = "";
+    strcat(shaf_file, main_file); strcat(shaf_file, ".shaf");
+    strcat(cod_file, main_file); strcat(cod_file, ".cod");
     FILE *in, *out;
-    in = fopen(filename, "rb");
-    out = fopen(strcat(filename, ".shaf"), "wb+");
+    in = fopen(cod_file, "rb");
+    out = fopen(shaf_file, "wb+");
 
-    print_final_info(start_time);
+    print_final_info(start_time, shaf_file);
 }
 
-void print_final_info(clock_t start_time){
+void print_final_info(clock_t start_time, char *shaf_file){
     clock_t stop_time = clock();
     double elapsed = (double)(stop_time-start_time)/CLOCKS_PER_SEC*1000;
     printf("André Vaz (a93221) e Rui Alves, MIEI/CD\n"); // TODO Add time
@@ -28,5 +33,5 @@ void print_final_info(clock_t start_time){
     printf("Tamanho antes/depois & taxa de compressão (bloco _): _/_\n"); // TODO
     printf("Taxa de compressão global: _%%\n"); // TODO
     printf("Tempo de execução do módulo: %fms\n", elapsed);
-    printf("Ficheiro gerado: _\n"); // TODO
+    printf("Ficheiro gerado: %s\n", shaf_file);
 }
--- src/moduloC/ModuloC.h
+++ src/moduloC/ModuloC.h
@@ -7,5 +7,17 @@ Main do ModuloC
 #include <time.h>
 #include <string.h>
 
+/**
+ * @brief Codificação de um ficheiro de símbolos
+ * 
+ * @param[in] filename Nome do ficheiro a codificar
+ */
 void moduloC(char *filename);
-void print_final_info();
+
+/**
+ * @brief Print no terminal a informação final de execução
+ * 
+ * @param[in] start_time Tempo de inicio de execução
+ * @param[in] shaf_file Nome do ficheiro de output
+ */
+void print_final_info(clock_t start_time, char *shaf_file);
