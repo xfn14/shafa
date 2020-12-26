b0ded71825877fb597fc26b61d2c17b79d055d9f
edumagalhaes10 <eduardomagalhaes107@gmail.com>
2020-12-26 18:17:52 +0000

    mod_f  (not finished)

---
+++ src/moduloF/moduloF.h
@@ -0,0 +1,15 @@
+#ifndef MOD_A_H
+#define MOD_A_H
+
+#include <stdio.h> 
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <time.h>
+#include "fsize.h"
+
+
+
+int main();
+
+#endif //MOD_A_H
