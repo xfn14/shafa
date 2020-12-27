bc84ca4303981a402b101d711146db9cfbbdf0ff
diogovieira63 <a93251@alunos.uminho.pt>
2020-12-27 00:11:59 +0000

    shaf decompression (kinda done), read cod file(done)

--- src/moduloD/d_array.h
+++ src/moduloD/d_array.h
@@ -3,15 +3,14 @@
 #define _D_ARRAY_
 
 typedef struct {
-  unsigned char *array;
-  int used;
-  int size;
+    unsigned char *array;
+    int used;
+    int size;
 } D_Array;
 
 void initArray(D_Array *a, size_t initialSize);
 void insertArray(D_Array *a, unsigned char element);
 void freeArray(D_Array *a);
-void remove_first (D_Array *a);
-void print_array (D_Array *a);
-
+void remove_first(D_Array *a);
+void print_array(D_Array *a, int mode);
 #endif
