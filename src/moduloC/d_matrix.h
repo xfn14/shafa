cba220467045a24fb848b3e9f18469ddbc6fcd6e
André Vaz <andre.vaz1411@gmail.com>
2020-12-29 00:12:24 +0000

    xixi cama

--- src/moduloC/d_matrix.h
+++ src/moduloC/d_matrix.h
@@ -6,10 +6,22 @@ typedef struct {
     int len;
 } D_Matrix;
 
+typedef struct {
+    D_Matrix *list;
+    int len;
+} D_Matrix_List;
+
 void initMatrix(D_Matrix *m);
 void addLineMatrix(D_Matrix *m, D_Array arr);
+void clearMatrix(D_Matrix *m);
 void printMatrix(D_Matrix *m);
 void freeMatrix(D_Matrix *m);
+
+void initMatrixList(D_Matrix_List *m_list);
+void insertMatrixInList(D_Matrix_List *m_list, D_Matrix m);
+void clearMatrixList(D_Matrix_List *m_list);
+void printMatrixList(D_Matrix_List *m_list);
+void freeMatrixList(D_Matrix_List *m_list);
 
 void initMatrix(D_Matrix *m){
     m->arr = malloc(2*sizeof(D_Array));
@@ -17,30 +29,89 @@ void initMatrix(D_Matrix *m){
 }
 
 void addLineMatrix(D_Matrix *m, D_Array arr){
-    m->len++;
-    m->arr = realloc(m->arr, (m->len+1)*sizeof(D_Matrix));
-
+    m->arr = realloc(m->arr, (m->len+2)*sizeof(D_Array));
     D_Array copy;
     initArray(&copy, arr.size);
     for(int i = 0; i < arr.used; i++){
         insertArray(&copy, arr.array[i]);
     }
+    copy.used = arr.used;
+    copy.size = arr.size;
 
     m->arr[m->len] = copy;
+    m->len++;
+}
+
+void clearMatrix(D_Matrix *m){
+    m->arr = realloc(m->arr, 1*sizeof(D_Array));
+    m->arr = NULL;
+    m->len = 0;
 }
 
 void printMatrix(D_Matrix *m){
+    int n = 0;
     for(int i = 0; i < m->len; i++){
         print_array(&m->arr[i], 0);
-        printf("\n");
+        n++;
+        if(n == 6){
+            n = 0;
+            printf("\n");
+        }else{
+            printf(" ");
+        }
+
     }
+    printf("\n");
 }
 
 void freeMatrix(D_Matrix *m){
-    for(int i = 0; i < m->len; i++){
-        freeArray(&m->arr[i]);
-    }
+//    for(int i = 0; i < m->len; i++){
+//        freeArray(&m->arr[i]);
+//    }
     free(m->arr);
     m->arr = NULL;
     m->len = 0;
+}
+
+void initMatrixList(D_Matrix_List *m_list){
+    m_list->list = malloc(1*sizeof(D_Matrix));
+    m_list->len = 0;
+}
+
+void insertMatrixInList(D_Matrix_List *m_list, D_Matrix m){
+    m_list->list = realloc(m_list->list, (m_list->len+2)*sizeof(D_Matrix));
+    m_list->list[m_list->len] = m;
+    m_list->len++;
+
+//    D_Matrix copy;
+//    initMatrix(&copy);
+//    for(int i = 0; i < m.len; i++){
+//        addLineMatrix(&copy, m.arr[i]);
+//    }
+//    copy.len = m.len;
+
+
+}
+
+void clearMatrixList(D_Matrix_List *m_list){
+    m_list->list = realloc(m_list->list, 1*sizeof(D_Matrix));
+    m_list->list = NULL;
+    m_list->len = 0;
+}
+
+void printMatrixList(D_Matrix_List *m_list){
+    for(int i = 0; i < m_list->len; i++){
+        printf("------------------------- Matrix %d (%d) -------------------------\n\n", i, m_list->list[i].len);
+        printMatrix(&m_list->list[i]);
+    }
+    printf("------------------------------------------------------------\n");
+}
+
+void freeMatrixList(D_Matrix_List *m_list){
+//    for(int i = 0; i < m_list->len; i++){
+//        freeMatrix(&m_list->list[i]);
+//    }
+    free(m_list->list);
+    m_list->list = NULL;
+    m_list->len = 0;
 }
---
+++ src/moduloD/README.md
@@ -0,0 +1,8 @@
+# Módulo D
+
+<!-- Insert information here -->
+
+## Autores
+
+- [Diogo Vieira](https://github.com/DiogoVieira63)
+- [António Fernandes](https://github.com/antoniolmf)
